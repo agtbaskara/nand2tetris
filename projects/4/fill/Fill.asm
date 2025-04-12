6fa15efa341a2533359affe1efe2ab43212c0668
agtbaskara <agtbaskara@gmail.com>
2025-04-12 19:48:12 +0900

    add project 4

--- projects/4/fill/Fill.asm
+++ projects/4/fill/Fill.asm
@@ -7,4 +7,65 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+
+(START)
+
+// i = 1
+@i
+M=0
+
+// max_loop = 8191 <= 256x512/16 - 1
+@8191
+D=A
+@max_loop
+M=D
+
+(LOOP)
+
+// if(i > max_loop) goto START
+@i
+D=M
+@max_loop
+D=D-M
+@START
+D;JGT
+
+//if(@KBD != 0) goto BLACK else goto WHITE
+@KBD
+D=M
+@BLACK
+D;JGT
+@WHITE
+0;JMP
+
+(BLACK)
+// *(@SCREEN + i) = -1
+@SCREEN
+D=A
+@i
+A=D+M
+M=-1
+
+// i = i + 1
+@i
+M=M+1
+
+// goto LOOP
+@LOOP
+0;JMP
+
+(WHITE)
+// *(@SCREEN + i) = 0
+@SCREEN
+D=A
+@i
+A=D+M
+M=0
+
+// i = i + 1
+@i
+M=M+1
+
+// goto LOOP
+@LOOP
+0;JMP
--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -6,4 +6,47 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+// i = 1
+@i
+M=1
+
+// sum = 0
+@sum
+M=0
+
+(LOOP)
+
+// if(i > R1) goto STOP
+@i
+D=M
+@R1
+D=D-M
+@STOP
+D;JGT
+
+// R2 = R2 + R0
+@sum
+D=M
+@R0
+D=D+M
+@sum
+M=D
+
+// i = i + 1
+@i
+M=M+1
+
+// goto LOOP
+@LOOP
+0;JMP
+
+(STOP)
+// R2 = sum
+@sum
+D=M
+@R2
+M=D
+
+(END)
+@END
+0;JMP
